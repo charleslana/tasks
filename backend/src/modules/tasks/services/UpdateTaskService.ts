7c75c2a559f1dfbf98e01ce2b1d4cf5b92365775
Charles <charleslanop@gmail.com>
2021-11-20 07:56:13 -0300

    refactoring

--- backend/src/modules/tasks/services/UpdateTaskService.ts
+++ backend/src/modules/tasks/services/UpdateTaskService.ts
@@ -7,20 +7,29 @@ import Task from '../typeorm/entities/Task';
 import { TaskRepository } from '../typeorm/repositories/TaskRepository';
 
 class UpdateTaskService {
+  private checkNotFound(task: Task | undefined) {
+    if (!task) {
+      throw new AppError('Tarefa não encontrada.');
+    }
+    return task;
+  }
+
+  private checkStatus(task: Task | undefined) {
+    if (task?.status === TaskStatusEnum.YES) {
+      throw new AppError(
+        'Não é possível atualizar uma tarefa que já se encontra finalizada.'
+      );
+    }
+  }
+
   public async execute({
     id,
     description,
   }: UpdateTaskInterface): Promise<Task> {
     const taskRepository = getCustomRepository(TaskRepository);
-    const task = await taskRepository.findOne(id);
-    if (!task) {
-      throw new AppError('Tarefa não encontrada.');
-    }
-    if (task.status === TaskStatusEnum.YES) {
-      throw new AppError(
-        'Não é possível atualizar uma tarefa que já se encontra finalizada.'
-      );
-    }
+    let task = await taskRepository.findOne(id);
+    task = this.checkNotFound(task);
+    this.checkStatus(task);
     const taskExists = await taskRepository.findByDescription(description);
     if (taskExists && description !== task.description) {
       throw new AppError('Já existe uma tarefa com a mesma descrição.');
@@ -32,13 +41,9 @@ class UpdateTaskService {
 
   public async finished({ id }: StatusInterface): Promise<Task> {
     const taskRepository = getCustomRepository(TaskRepository);
-    const task = await taskRepository.findOne(id);
-    if (!task) {
-      throw new AppError('Tarefa não encontrada.');
-    }
-    if (task.status === TaskStatusEnum.YES) {
-      throw new AppError('A tarefa já se encontra finalizada.');
-    }
+    let task = await taskRepository.findOne(id);
+    task = this.checkNotFound(task);
+    this.checkStatus(task);
     task.status = TaskStatusEnum.YES;
     await taskRepository.save(task);
     return task;
